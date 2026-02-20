7e62b3ed94406d882fc29ad70edcdc17e733785a
Marcos De La Cruz <marcosdelacruz2020@gmail.com>
2026-02-20 14:27:35 -0600

    Add gh-pages as a devDependency in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,5 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({site: 'https://MARCOSDELACRUZ2021.github.io',
+  base: '/nazar-web/',});
