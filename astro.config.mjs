edc5dc4bc3150c2aaa0b929945ef8451e1f651da
Marcos De La Cruz <marcosdelacruz2020@gmail.com>
2026-02-20 18:44:26 -0600

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({site: 'https://MARCOSDELACRUZ2021.github.io',
-  base: '/nazar-web/',});
+export default defineConfig({});
